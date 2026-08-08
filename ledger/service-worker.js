0d11a36e46f008463806a4a43958c07d3674a5a1
五 <fuhei702@gmail.com>
2026-08-08 22:44:30 +0700

    Add files via upload

--- ledger/service-worker.js
+++ ledger/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "abin-ledger-v3";
+const CACHE_NAME = "abin-ledger-v5";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
